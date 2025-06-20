fdb5347f58f530f6d87929533a652557b3ced7e6
Svetlana <112646144+cryptoanalyst834@users.noreply.github.com>
2025-06-20 13:26:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,10 @@
-from aiogram import Bot, Dispatcher
 import os
-
-bot = Bot(token=os.getenv("BOT_TOKEN"))
-dp = Dispatcher()import os
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
-from handlers import router  # импортируем маршруты
+from handlers import router  # обязательно
 
-# Инициализация бота
+# Инициализация бота и диспетчера
 bot = Bot(token=os.getenv("BOT_TOKEN"), parse_mode=ParseMode.HTML)
 dp = Dispatcher(storage=MemoryStorage())
 
