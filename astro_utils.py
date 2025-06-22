1fc0ca378166ad545a23cccc61adf5431496ecb2
Svetlana <112646144+cryptoanalyst834@users.noreply.github.com>
2025-06-22 19:35:13 +0300

    Update astro_utils.py

--- astro_utils.py
+++ astro_utils.py
@@ -1,19 +1,13 @@
 from flatlib.chart import Chart
 from flatlib.datetime import Datetime
 from flatlib.geopos import GeoPos
+from datetime import datetime
+from geopy.geocoders import Nominatim
 
 def generate_natal_chart(date: str, time: str, location: str = 'Moscow'):
-    """Возвращает знак Солнца и Асцендента на момент рождения"""
-    from datetime import datetime
-    from geopy.geocoders import Nominatim
-
-    # Преобразование строки времени
     dt = datetime.strptime(f"{date} {time}", "%d.%m.%Y %H:%M")
-
-    # Геопозиция по умолчанию
     geo = GeoPos('55.7558', '37.6176')  # Москва
 
-    # Геокодинг, если указан город
     if location and location.lower() != 'moscow':
         try:
             geolocator = Nominatim(user_agent="astro_bot")
@@ -21,12 +15,8 @@ def generate_natal_chart(date: str, time: str, location: str = 'Moscow'):
             if loc:
                 geo = GeoPos(str(loc.latitude), str(loc.longitude))
         except:
-            pass  # если геокодинг не сработал, оставим Москву
+            pass
 
     date_obj = Datetime(dt.strftime('%Y/%m/%d'), dt.strftime('%H:%M'), '+03:00')
     chart = Chart(date_obj, geo)
-
-    sun_sign = chart.get('SUN').sign
-    asc_sign = chart.get('ASC').sign
-
-    return sun_sign, asc_sign
+    return chart.get('SUN').sign, chart.get('ASC').sign
