09b7894573776a573c668542f2621e79cff316c5
Svetlana <112646144+cryptoanalyst834@users.noreply.github.com>
2025-06-22 19:50:59 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,25 +1,14 @@
 from sqlalchemy import Column, Integer, String
-from database import Base
-from pydantic import BaseModel
+from db_base import Base  # импортируем из нового файла
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = 'users'
+
     id = Column(Integer, primary_key=True, index=True)
-    telegram_id = Column(Integer, unique=True)
     name = Column(String)
     birth_date = Column(String)
     birth_time = Column(String)
     birth_place = Column(String)
-    photo_id = Column(String)
     sun_sign = Column(String)
     asc_sign = Column(String)
-
-class UserCreate(BaseModel):
-    telegram_id: int
-    name: str
-    birth_date: str
-    birth_time: str
-    birth_place: str
-    photo_id: str
-    sun_sign: str
-    asc_sign: str
+    photo_id = Column(String)
