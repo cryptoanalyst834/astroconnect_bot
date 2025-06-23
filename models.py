fa644d500d925b1eb7ef263200477cb133724344
Svetlana <112646144+cryptoanalyst834@users.noreply.github.com>
2025-06-23 09:23:58 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,14 +1,16 @@
 from sqlalchemy import Column, Integer, String
-from db_base import Base  # импортируем из нового файла
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
 
 class User(Base):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
+    telegram_id = Column(String, unique=True, index=True)
     name = Column(String)
     birth_date = Column(String)
     birth_time = Column(String)
     birth_place = Column(String)
     sun_sign = Column(String)
     asc_sign = Column(String)
-    photo_id = Column(String)
