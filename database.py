09dddda2f06545ad7a61fcf849a6fccb3da20699
Svetlana <112646144+cryptoanalyst834@users.noreply.github.com>
2025-06-23 09:24:48 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,17 +1,25 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
+from models import Base
 import os
+from dotenv import load_dotenv
 
-# Создание базы
-DATABASE_URL = os.getenv("DATABASE_URL")  # Убедись, что переменная задана в Railway
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(DATABASE_URL, echo=True)
-async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+AsyncSessionLocal = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
-# Базовый класс для моделей
-Base = declarative_base()
-
-# Функция для инициализации базы
 async def create_db_and_tables():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+
+async def save_user(user_data):
+    async with AsyncSessionLocal() as session:
+        session.add(user_data)
+        await session.commit()
+
+async def get_all_users():
+    async with AsyncSessionLocal() as session:
+        result = await session.execute("SELECT * FROM users")
+        return result.fetchall()
