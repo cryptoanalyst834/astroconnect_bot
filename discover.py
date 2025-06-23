e9655423c53e5aefcbfe81b3cd283e2d9c5f10b2
Svetlana <112646144+cryptoanalyst834@users.noreply.github.com>
2025-06-23 09:29:26 +0300

    Delete discover.py

--- discover.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from database import get_all_users
-
-router = APIRouter()
-
-@router.get("/profiles")
-async def profiles():
-    users = await get_all_users()
-    return users
