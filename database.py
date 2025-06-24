308c55b02a5507dc40415e89a7e4381748e543da
Svetlana <112646144+cryptoanalyst834@users.noreply.github.com>
2025-06-24 21:28:14 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,9 +1,15 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
-from config import DATABASE_URL
 
-engine = create_async_engine(DATABASE_URL, echo=True)
-async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+from config import DATABASE_URL       # ← импорт строки подключения
+
+# Настраиваем движок и сессии
+engine = create_async_engine(DATABASE_URL, echo=False)
+async_session_maker = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
 Base = declarative_base()
 
 async def init_db():
