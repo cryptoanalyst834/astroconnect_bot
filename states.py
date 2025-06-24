725e6ac89b82acf577f2ec53744bd241ad43e3a6
Svetlana <112646144+cryptoanalyst834@users.noreply.github.com>
2025-06-24 18:46:22 +0300

    Update states.py

--- states.py
+++ states.py
@@ -1,8 +1,6 @@
 from aiogram.fsm.state import StatesGroup, State
 
-class RegistrationState(StatesGroup):
-    name = State()
-    birth_date = State()
-    birth_time = State()
-    birth_place = State()
-    photo = State()
+class ProfileStates(StatesGroup):
+    waiting_for_birthdate = State()
+    waiting_for_birthtime = State()
+    waiting_for_location = State()
