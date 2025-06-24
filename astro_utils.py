e4e2851c0d6028a66adffeadae8ea9923116745b
Svetlana <112646144+cryptoanalyst834@users.noreply.github.com>
2025-06-24 21:28:44 +0300

    Update astro_utils.py

--- astro_utils.py
+++ astro_utils.py
@@ -2,7 +2,19 @@ from flatlib.chart import Chart
 from flatlib.datetime import Datetime
 from flatlib.geopos import GeoPos
 
-def get_natal_chart(date_str, time_str, lat, lon):
-    date = Datetime(f'{date_str}', f'{time_str}')
-    pos = GeoPos(lat, lon)
-    return Chart(date, pos)
+# Здесь config не нужен, но при желании можно
+# импортировать таймзону или прочие константы
+
+def generate_astrology_data(date_str: str, time_str: str, place: str):
+    date_parts = date_str.split(".")
+    dt = Datetime(
+        f"{date_parts[2]}-{date_parts[1]}-{date_parts[0]}",
+        time_str,
+        "+03:00",    # можно тоже вынести в config.TZ
+    )
+    pos = GeoPos(*get_coordinates(place))
+    chart = Chart(dt, pos)
+    return {
+        "zodiac_sign": chart.get("SUN").sign,
+        "ascendant": chart.get("ASC").sign
+    }
