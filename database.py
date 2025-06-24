04bf6a5125c67aa0a3f21dc62f6deeecc01de66b
Svetlana <112646144+cryptoanalyst834@users.noreply.github.com>
2025-06-24 18:48:25 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,20 +1,10 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.pool import NullPool
-from fastapi import Depends
-import os
+from config import DATABASE_URL
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-engine = create_async_engine(DATABASE_URL, echo=False, poolclass=NullPool)
+engine = create_async_engine(DATABASE_URL, echo=True)
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
-
 Base = declarative_base()
-
-async def get_async_session() -> AsyncSession:
-    async with async_session_maker() as session:
-        yield session
 
 async def init_db():
     async with engine.begin() as conn:
