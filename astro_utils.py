00c51c12ccb4ee203f98aa040fe43f6c79850da1
Svetlana <112646144+cryptoanalyst834@users.noreply.github.com>
2025-06-24 09:24:29 +0300

    Update astro_utils.py

--- astro_utils.py
+++ astro_utils.py
@@ -1,21 +1,19 @@
 from flatlib.chart import Chart
 from flatlib.datetime import Datetime
 from flatlib.geopos import GeoPos
-from datetime import datetime
-from geopy.geocoders import Nominatim
 
-def generate_natal_chart(date: str, time: str, location: str = 'Moscow'):
-    geo = GeoPos('55.7558', '37.6176')  # Москва
+def generate_natal_chart(date_str, time_str, city):
+    # В проде подключить геокодинг через API или использовать словарь
+    pos = GeoPos("55n45", "37e35")  # Москва по умолчанию
+    date_parts = date_str.split(".")
+    year, month, day = int(date_parts[2]), int(date_parts[1]), int(date_parts[0])
+    dt = Datetime(f"{year}-{month:02}-{day:02}", time_str, "+03:00")
 
-    if location.lower() != 'moscow':
-        try:
-            geolocator = Nominatim(user_agent="astro_bot")
-            loc = geolocator.geocode(location)
-            if loc:
-                geo = GeoPos(str(loc.latitude), str(loc.longitude))
-        except:
-            pass
+    chart = Chart(dt, pos)
+    sun = chart.get("SUN")
+    asc = chart.get("ASC")
 
-    date_obj = Datetime(datetime.strptime(date, '%d.%m.%Y').strftime('%Y/%m/%d'), time, '+03:00')
-    chart = Chart(date_obj, geo)
-    return chart.get('SUN').sign, chart.get('ASC').sign
+    return {
+        "zodiac": sun.sign,
+        "ascendant": asc.sign
+    }
