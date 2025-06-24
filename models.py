b14d0707ff935f619460628e6dd760449d93301d
Svetlana <112646144+cryptoanalyst834@users.noreply.github.com>
2025-06-24 11:41:55 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,30 +1,13 @@
 from sqlalchemy import Column, Integer, String
-from database import Base
+from db_base import Base
 
 class UserProfile(Base):
     __tablename__ = "user_profiles"
 
     id = Column(Integer, primary_key=True, index=True)
-    telegram_id = Column(Integer)
-    name = Column(String)
-    birth_date = Column(String)
-    birth_time = Column(String)
-    birth_place = Column(String)
-    photo_id = Column(String)
-    photo_url = Column(String)
+    telegram_id = Column(String, unique=True, index=True)
+    username = Column(String)
+    about = Column(String)
+    photo_path = Column(String)
     zodiac = Column(String)
     ascendant = Column(String)
-
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "telegram_id": self.telegram_id,
-            "name": self.name,
-            "birth_date": self.birth_date,
-            "birth_time": self.birth_time,
-            "birth_place": self.birth_place,
-            "photo_id": self.photo_id,
-            "photo_url": self.photo_url,
-            "zodiac": self.zodiac,
-            "ascendant": self.ascendant,
-        }
