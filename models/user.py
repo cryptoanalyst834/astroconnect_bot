5e58a59c29c40640a8112ef0d3f6d2a03a869442
Svetlana <112646144+cryptoanalyst834@users.noreply.github.com>
2025-06-24 21:11:57 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,1 +1,8 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
 
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    telegram_id = Column(Integer, unique=True, index=True)
+    name = Column(String)
