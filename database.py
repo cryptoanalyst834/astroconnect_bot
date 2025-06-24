7a5bdad32ccc9174ab439c9c271a48d5e8d39ae9
Svetlana <112646144+cryptoanalyst834@users.noreply.github.com>
2025-06-24 11:42:27 +0300

    Update database.py

--- database.py
+++ database.py
@@ -2,6 +2,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import select
 from models import UserProfile
+from db_base import Base
 import os
 from dotenv import load_dotenv
 
@@ -11,18 +12,15 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_async_engine(DATABASE_URL, echo=False)
 async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
-# Это и есть get_session — генератор сессии
 async def get_session() -> AsyncSession:
     async with async_session() as session:
         yield session
 
-# Добавление анкеты в базу
 async def add_user_profile(session: AsyncSession, data: dict):
     profile = UserProfile(**data)
     session.add(profile)
     await session.commit()
 
-# Получение всех анкет
 async def get_all_profiles(session: AsyncSession):
     result = await session.execute(select(UserProfile))
     return result.scalars().all()
