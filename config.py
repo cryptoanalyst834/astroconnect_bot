23c37286810ed7ef7bc77310842c33265db628df
Svetlana <112646144+cryptoanalyst834@users.noreply.github.com>
2025-06-25 09:16:51 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ load_dotenv()
 # Теперь эти переменные можно импортировать в любой модуль
 TOKEN        = os.getenv("TOKEN")
 DATABASE_URL = os.getenv("DATABASE_URL")
+FRONTEND_URL = os.getenv("FRONTEND_URL", "https://685798b95e51acca36207efb--astroconnectminiapp.netlify.app/")
 
 if not TOKEN or not DATABASE_URL:
     raise RuntimeError("Не установлены обязательные переменные окружения: TOKEN и DATABASE_URL")
