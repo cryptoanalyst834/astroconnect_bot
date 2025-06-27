a125d367af0b1e9df69c5583bd2011a0ed71f883
Svetlana <112646144+cryptoanalyst834@users.noreply.github.com>
2025-06-27 09:04:32 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -3,12 +3,12 @@ from database import Base
 
 class UserProfile(Base):
     __tablename__ = "profiles"
-    id           = Column(Integer, primary_key=True, index=True)
-    telegram_id  = Column(Integer, unique=True, index=True)
-    name         = Column(String)
-    birth_date   = Column(String)
-    birth_time   = Column(String)
-    birth_place  = Column(String)
-    zodiac       = Column(String)
-    ascendant    = Column(String)
-    photo_id     = Column(String)
+    id = Column(Integer, primary_key=True, index=True)
+    telegram_id = Column(Integer, unique=True, index=True)
+    name = Column(String)
+    birth_date = Column(String)
+    birth_time = Column(String)
+    birth_place = Column(String)
+    zodiac = Column(String)
+    ascendant = Column(String)
+    photo_id = Column(String)
