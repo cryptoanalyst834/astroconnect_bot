e57787482450f3d3739c2eafd070c3b014675c1b
Svetlana <112646144+cryptoanalyst834@users.noreply.github.com>
2025-06-28 08:40:38 +0300

    Update astro_utils.py

--- astro_utils.py
+++ astro_utils.py
@@ -1,11 +1,12 @@
 from flatlib.chart import Chart
 from flatlib.datetime import Datetime
 from flatlib.geopos import GeoPos
+from flatlib.const import SUN, ASC
 
-def calc_natal(date_str, time_str, place_lat, place_lon):
-    dt = Datetime(date_str, time_str, "+03:00")  # поправьте на нужный TZ
+def calc_chart(date_str, time_str, place_lat, place_lon):
+    dt = Datetime(f"{date_str}", f"{time_str}")
     pos = GeoPos(place_lat, place_lon)
     chart = Chart(dt, pos)
-    sun_sign = chart.get("SUN").sign
-    asc_sign = chart.get("ASC").sign
-    return sun_sign, asc_sign
+    zodiac = chart.get(SUN).sign
+    ascendant = chart.get(ASC).sign
+    return zodiac, ascendant
