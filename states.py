34e776a83b78a0d113bc590f1f70fb8abd32f252
Svetlana <112646144+cryptoanalyst834@users.noreply.github.com>
2025-06-28 11:31:58 +0300

    Update states.py

--- states.py
+++ states.py
@@ -1,11 +1,8 @@
 from aiogram.fsm.state import StatesGroup, State
-from aiogram.fsm.storage.memory import MemoryStorage
 
-storage = MemoryStorage()
-
-class ProfileStates(StatesGroup):
-    name        = State()
-    birth_date  = State()
-    birth_time  = State()
+class RegistrationStates(StatesGroup):
+    name = State()
+    birth_date = State()
+    birth_time = State()
     birth_place = State()
-    photo       = State()
+    photo_id = State()
