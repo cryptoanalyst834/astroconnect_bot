679d7b3278a1e9e9f7f92442718d1cdbae05d6d0
Svetlana <112646144+cryptoanalyst834@users.noreply.github.com>
2025-06-29 08:31:13 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,15 +1,14 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Date, Time
 from database import Base
 
 class UserProfile(Base):
     __tablename__ = "profiles"
-
     id = Column(Integer, primary_key=True, index=True)
-    telegram_id = Column(Integer, unique=True, index=True)
+    tg_id = Column(String, unique=True, index=True)
     name = Column(String)
-    birth_date = Column(String)
-    birth_time = Column(String)
+    birth_date = Column(Date)
+    birth_time = Column(Time)
     birth_place = Column(String)
-    photo_id = Column(String)
     zodiac = Column(String)
     ascendant = Column(String)
+    photo_id = Column(String, nullable=True)
