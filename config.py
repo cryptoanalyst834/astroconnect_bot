808ac9349233682089f14a813010a039633c11b6
Svetlana <112646144+cryptoanalyst834@users.noreply.github.com>
2025-06-29 08:32:19 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,15 +6,12 @@ load_dotenv()
 TOKEN = os.getenv("TOKEN")
 if not TOKEN:
     raise RuntimeError("Переменная TOKEN не задана!")
-
 RAILWAY_APP_URL = os.getenv("RAILWAY_APP_URL")
 if not RAILWAY_APP_URL:
     raise RuntimeError("Переменная RAILWAY_APP_URL не задана!")
-
 FRONTEND_URL = os.getenv("FRONTEND_URL")
 if not FRONTEND_URL:
     raise RuntimeError("Переменная FRONTEND_URL не задана!")
-
 DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
-    raise RuntimeError("Переменная DATABASE_URL не задана!")
+    raise RuntimeError("DATABASE_URL не задана в окружении!")
