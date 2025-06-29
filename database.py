c16737f53bbe69ad3730fb4f3c77d01c1db7336e
Svetlana <112646144+cryptoanalyst834@users.noreply.github.com>
2025-06-29 08:31:48 +0300

    Update database.py

--- database.py
+++ database.py
@@ -7,15 +7,8 @@ from contextlib import asynccontextmanager
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-if not DATABASE_URL:
-    raise RuntimeError("DATABASE_URL не задана в окружении!")
-
-engine = create_async_engine(DATABASE_URL, echo=True, pool_pre_ping=True)
-
-AsyncSessionLocal = sessionmaker(
-    engine, class_=AsyncSession, expire_on_commit=False
-)
-
+engine = create_async_engine(DATABASE_URL, echo=False, pool_pre_ping=True)
+AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 Base = declarative_base()
 
 async def init_db():
