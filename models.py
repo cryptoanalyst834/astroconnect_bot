63c5d85fd5e3678c21ce243477c1ae075c6944d7
Svetlana <112646144+cryptoanalyst834@users.noreply.github.com>
2025-06-30 10:03:27 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,19 +1,20 @@
-from sqlalchemy import Column, Integer, String, Date, Float
+from sqlalchemy import Column, Integer, String, Date, Time, Float
 from sqlalchemy.dialects.postgresql import JSONB
 from database import Base
 
 class UserProfile(Base):
     __tablename__ = "profiles"
-    id = Column(Integer, primary_key=True, index=True)
-    tg_id = Column(Integer, unique=True, index=True, nullable=False)
-    name = Column(String, nullable=False)
-    gender = Column(String)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    telegram_id = Column(String, unique=True, index=True, nullable=False)
+    name = Column(String)
     birth_date = Column(Date)
-    birth_time = Column(String)
+    birth_time = Column(Time)
     birth_place = Column(String)
     latitude = Column(Float)
     longitude = Column(Float)
+    gender = Column(String)
+    description = Column(String)
+    photo_id = Column(String)
     zodiac = Column(String)
     ascendant = Column(String)
-    description = Column(String)
-    astro_data = Column(JSONB)
+    natal_chart = Column(JSONB)
