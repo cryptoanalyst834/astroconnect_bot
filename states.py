13efccc37d85ef9e1f0dd48f2124a87dfcece824
Svetlana <112646144+cryptoanalyst834@users.noreply.github.com>
2025-07-07 16:05:19 +0300

    Delete states.py

--- states.py
+++
@@ -1,9 +0,0 @@
-from aiogram.fsm.state import StatesGroup, State
-
-class RegistrationStates(StatesGroup):
-    name = State()
-    birth_date = State()
-    birth_time = State()
-    birth_city = State()   
-    about = State()
-    photo = State()
