252e493f33e17616d876bbb6a8eba4a6137fa0c3
Svetlana <112646144+cryptoanalyst834@users.noreply.github.com>
2025-07-08 18:36:53 +0300

    Update package.js

--- package.js
+++ package.js
@@ -1,6 +1,20 @@
 {
-  "name": "astroconnect-bot",
+  "name": "astroconnect-backend",
   "version": "1.0.0",
   "type": "module",
-  "main": "server.js
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "mongoose": "^7.6.1",
+    "dotenv": "^16.3.1",
+    "cors": "^2.8.5",
+    "morgan": "^1.10.0",
+    "node-fetch": "^3.3.2",
+    "flatlib": "^1.0.1",
+    "telegram-bot-api": "^2.0.1",
+    "multer": "^1.4.5-lts.1"
+  }
 }
